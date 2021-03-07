b31093a65195f3760c817ebaa9961a7f1c4954bf
Klaudia Rydzanicz (CODECOOL POLAND SP. Z O.O.) <klaudia.jamrozik@gmail.com>
2021-03-07 15:52:23 +0100

    The implementation of TODOO. Add encodeURI function and IIFE.

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-const apiUrl = "https://www.ryanair.com/api/farfnd/3/oneWayFares?&departureAirportIataCode=KRK&language=pl";
+const apiUrl = "https://www.ryanair.com/api/farfnd/3/";
 
-async function getFlights(
-    limit = 30,
-    market = "pl-pl",
-    priceValue = 350,
-    departureDateFrom = "2021-03-10",
-    departureDateTo = "2022-03-02") {
-    // TODO create separate method for generating url with queryParams
-    const url = `${apiUrl}&limit=${limit}&market=${market}` +
+async function getFlights(limit = 30, market = "pl-pl", priceValue = 350, departureDateFrom = "2021-03-10",
+    departureDateTo = "2022-03-02", iataCode = "KRK", language = "pl", fares = "oneWayFares") {
+    const queryParams = `${fares}?&departureAirportIataCode=${iataCode}` +
+        `&language=${language}&limit=${limit}&market=${market}` +
         `&offset=0&outboundDepartureDateFrom=${departureDateFrom}` +
         `&outboundDepartureDateTo=${departureDateTo}&priceValueTo=${priceValue}`;
+    
+    const url = encodeURI(apiUrl + queryParams);
     const response = await fetch(url);
     if (response.ok) {
         return await response.json();
@@ -17,17 +15,15 @@ async function getFlights(
     throw new Error(`Server send ${response.statusText}`);
 }
 
-// TODO IIFE 
-getFlights()
-    .then(response => { getFlightsByTime(response) })
-    .catch(err => { console.error(err) });
+(function () {
+    getFlights()
+        .then(response => { getFlightsByTime(response) })
+        .catch(err => console.error(err));
+})();
 
 function getFlightsByTime(response) {
     const flightsByTime = [];
-    const regex = /T((1[0-9])|0[7-9]|(2[0-2]))/gi;
-
-    // let departure = new Date(element.outbound.departureDate)
-    // departure.getHours() > 7 || departure.getHours() < 22
+    const regex = /T((1[0-9])|0[7-9]|(2[0-2]))/gi; // let departure = new Date(element.outbound.departureDate) departure.getHours() > 7 || departure.getHours() < 22
 
     response.fares.filter(element => {
         if (element.outbound.departureDate.match(regex)) flightsByTime.push(element);
@@ -43,6 +39,3 @@ function displayFlights(elements) {
         body.appendChild(text);
     }
 }
-
-// IIFE
-getFlights();
